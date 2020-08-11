a881abd92f43e21cacf10cf8f4898351d5db48b5
sara <saronah1099@gmail.com>
2020-08-11 14:35:18 +0300

    removed sudo

--- s.sh
+++ s.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt update
-sudo apt install docker.io
-sudo apt install wget
+apt update
+# sudo apt install docker.io
+apt install wget
 wget https://releases.hashicorp.com/packer/0.9.0/packer_0.9.0_linux_386.zip
-sudo apt-get install unzip
+apt-get install unzip
 unzip packer_0.9.0_linux_386.zip
 export PATH=$PATH:~/packer/
 packer
