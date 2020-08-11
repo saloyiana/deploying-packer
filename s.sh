dcb9979772294a2bfcd13ebc670618693f9e5f58
sara <saronah1099@gmail.com>
2020-08-11 14:41:36 +0300

    add -y

--- s.sh
+++ s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt update -y
+sudo apt update -y
 # sudo apt install docker.io
-apt install wget -y
+sudo apt install wget -y
 wget https://releases.hashicorp.com/packer/0.9.0/packer_0.9.0_linux_386.zip -y
 apt-get install unzip -y 
 unzip packer_0.9.0_linux_386.zip -y
