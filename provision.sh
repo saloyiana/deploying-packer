c9a6a8230d07bc7161de2dbc7e15bcc9e03c8e50
sara <saronah1099@gmail.com>
2020-08-13 11:51:43 +0300

    fixed

--- provision.sh
+++ provision.sh
@@ -4,6 +4,7 @@ sudo usermod -aG docker ubuntu
 cd /home/ubuntu
 mkdir nginx_home
 chmod a+rwx nginx_home
+sudo chown ubuntu:ubuntu /home/ubuntu/nginx_home
 sudo touch /etc/systemd/system/nginx.service
 sudo chmod a+rw /etc/systemd/system/nginx.service
 sudo docker pull nginx
@@ -13,11 +14,11 @@ Description=Nginx
 After=docker.service
 [Service]
 Type=simple
-ExecStart=sudo docker container run -d --name nginx -p 80:8080 --restart=always -v /home/ubuntu/nginx_home:/var/nginx_home -v /run/docker.sock:/run/docker.sock nginx
+ExecStart=sudo docker container run -d --name nginx -p 80:8080 --restart=always -v /home/ubuntu/nginx_home/:/usr/share/nginx/html:ro nginx
 [Install]
 WantedBy=multi-user.target
 EOF
-sudo chmod a-w /etc/systemd/system/nginx.service
+sudo chmod a+r /etc/systemd/system/nginx.service
 sudo systemctl daemon-reload
 sudo systemctl enable nginx
-sudo chmod a+rw /run/docker.sock
+sudo systemctl start nginx
