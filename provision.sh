5891b48a87dc889d11bf2661da1b4af95f361290
sara <saronah1099@gmail.com>
2020-08-13 10:44:30 +0300

    edited

--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
-docker run --name nginx -it -p 8080:80 nginx
+docker run --name nginx -d -p 8080:80 nginx
