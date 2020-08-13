616018f284ecdbb7833e1e033374662971896e43
sara <saronah1099@gmail.com>
2020-08-13 10:05:03 +0300

    fixed provsioner

--- provision.sh
+++ provision.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
-docker run --name nginx -d -p 8080:80 nginx
-cat ip.txt > /usr/share/nginx/html/index.html
+docker run --name nginx -it -p 8080:80 nginx
