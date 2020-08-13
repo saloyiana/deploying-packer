7ca5687e8a6ea9985601fef174135f757545b302
sara <saronah1099@gmail.com>
2020-08-13 11:10:39 +0300

    systemd

--- provision.sh
+++ provision.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
-docker run --name nginx -d -p 8080:80 nginx
+cd /home/ubuntu
+mkdir nginx_home
+sudo docker pull nginx
+cat <<EOF > /etc/systemd/system/nginx.service
+[Unit]
+Description=Nginx 
+After=docker.service
+[Service]
+Type=simple
+ExecStart=sudo docker container run -d --name nginx -p 80:8080 --restart=always -v /home/ubuntu/nginx_home:/var/nginx_home -v /run/docker.sock:/run/docker.sock nginx
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo chmod a-w /etc/systemd/system/nginx.service
+sudo systemctl daemon-reload
+sudo systemctl enable nginx
+sudo chmod a+rw /run/docker.sock
