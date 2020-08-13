5cd64442588c2c4a41ff738e3e5852bde4ff0a42
sara <saronah1099@gmail.com>
2020-08-13 09:52:15 +0300

    add nginx

--- provision.sh
+++ provision.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
+docker run --name nginx -d -p 8080:80 nginx
+cat ip.txt > /usr/share/nginx/html/index.html
